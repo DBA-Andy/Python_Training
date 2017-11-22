24502ea919f2be5940599990dd9bab548c35ba28
DBA-Andy <33224955+DBA-Andy@users.noreply.github.com>
2017-11-21 21:55:16 -0500

    Playing with Loops updated

--- playingWithLoops.py
+++ playingWithLoops.py
@@ -1,10 +1,11 @@
 import time
 counter = 0
+limit = 1000000
 start_time = time.time()
-while counter < 1000000000:
+while counter < limit:
     print ("The wretched counter is: ", counter)
     counter = counter + 5
-print ("The elapsed time to count to 1000000000 is: ", time.time() - start_time)
+print ("The elapsed time to count to", limit, "is: ", time.time() - start_time)
 	
 start_time = time.time()
 alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']

a76a3bf0ff5705c4b43b2b266ebd6a2e3a4a65dd
DBA-Andy <33224955+DBA-Andy@users.noreply.github.com>
2021-02-19 17:25:55 -0500

    initial commit

---
+++ playingWithMutiThreading.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+#Import Modules
+from multiprocessing import Pool
+
+def main():
+    v_message="Main Procedure is starting"
+    print ("INFO:  " + v_message)
+
+if __name__ == "__main__":
+    main()

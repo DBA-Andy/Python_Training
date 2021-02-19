61fe01dd68157f2eb172d180e06ca2d0d4401ad2
DBA-Andy <33224955+DBA-Andy@users.noreply.github.com>
2021-02-19 17:38:55 -0500

    changing counters

--- playingWithMutiThreading.py
+++ playingWithMutiThreading.py
@@ -7,7 +7,7 @@ def while_loop(limit, counter=0):
     numbers = []
     while counter <= limit:
          ''' Iterate through a meaningless list, at the end of which we'll print how long it took, and the contents of an arbitrary list we created '''
-         print ("The wretched counter is: ", counter)
+        #print ("The wretched counter is: ", counter)
          numbers.append(counter)
          counter = counter + 1
     #print ("The contents of the Numbers list are: ", numbers)
@@ -17,8 +17,8 @@ def main():
     v_message="Main Procedure is starting"
     print ("INFO:  " + v_message)
 
-    limits=[1000,2000,3000,4000,5000,6000,7000,8000,9000,10000,11000,12000,13000,14000,15000,16000,17000,18000,19000,20000,21000,22000,23000,24000,25000,26000,27000,28000,29000,30000,31000,32000,33000,34000,35000,36000, \
-        37000,38000,39000,40000]
+    limits=[10000,20000,30000,40000,50000,60000,70000,80000,90000,100000,110000,120000,130000,140000,150000,160000,170000,180000,190000,200000,210000,220000,230000,240000,250000,260000,270000,280000,290000,300000,310000,320000,330000,340000,350000,360000, \
+        370000,380000,390000,400000]
 
     with Pool(5) as pool:
         pool.map(while_loop,limits)
